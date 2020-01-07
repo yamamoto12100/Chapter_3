7791f63e6ce3cdbb862d75fb319af6a781ac4f80
山本”git config --global user.name 山本” <t.ryyr.3840@gmail.com>
2020-01-07 01:40:04 +0000

    第3章課題

---
+++ 03_課題3.php
@@ -0,0 +1,6 @@
+<?php
+$fruits = ["apple","lemon","banana","grape","cherry"];
+foreach($fruits as $fruits){
+    echo "要素は" . $fruits;
+    echo "\n";
+}
---
+++ 03_課題4.php
@@ -0,0 +1,11 @@
+<?php
+
+$start = 1;
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+  if($i % 5 == 0){
+    echo $i;
+    echo "\n";
+  }
+}
